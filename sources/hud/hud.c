33708a1ed8b2e3d8a87bcb29fb7ec723ba8ef694
Jean-Baptiste Brousse <jbrousse@paul-f5Br6s4.clusters.42paris.fr>
2024-05-31 14:32:09 +0200

    basic map ok

--- sources/hud/hud.c
+++ sources/hud/hud.c
@@ -6,35 +6,53 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:54:21 by jbrousse          #+#    #+#             */
-/*   Updated: 2024/05/30 17:40:26 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/05/31 14:30:01 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "hud.h"
 
+t_vector2	rotate_point(t_vector2 point, float angle)
+{
+	t_vector2	result;
+
+	result.x = point.x * cos(angle) + point.y * sin(angle);
+	result.y = point.x * sin(angle) - point.y * cos(angle);
+	return (result);
+}
+
 void	update_minimap(t_texture *minimap, t_player *player)
 {
-	t_vector2	dst;
+	t_vector2	dst_offset;
 	t_vector2	size;
 	t_vector2	iter;
+	t_vector2	dst;
 	int			color;
 
-	(void)player;
-	dst = vector2(50, 50);
+	dst_offset = vector2(50, 50);
 	size = vector2(100, 100);
 	iter = vector2(0, 0);
+	float angle = atan2f(player->dir.y, -player->dir.x);
 	while (iter.y < size.y)
 	{
 		iter.x = 0;
 		while (iter.x < size.x)
 		{
-			color = take_pixel(minimap,
-					vector2(
-						(iter.x + (player->pos.y * 10) + (size.x / 2)),
-						(iter.y + (player->pos.x * 10) - (size.y / 2)))
-					);
-			pixel_put(__get_engine()->win[0], vector2(dst.x + iter.x, dst.y + iter.y), color);
+			t_vector2 tmp = rotate_point(vector2(iter.x - size.x / 2, iter.y - size.y / 2), angle);		
+
+			tmp.x += (player->pos.y * 10);
+			tmp.y += (player->pos.x * -10) + size.y / 2;
+			
+			if (tmp.x < 0 || tmp.x >= minimap->size.x
+				|| tmp.y < 0 || tmp.y >= minimap->size.y)
+				color = 0x000000;
+			else
+				color = take_pixel(minimap, tmp);
+			if (iter.x >= size.x / 2 - 3 && iter.y >= size.y / 2 - 3 && iter.x <= size.x / 2 + 3 && iter.y <= size.y / 2 + 3)
+				color = 0x00FFFF;
+			dst = vector2(dst_offset.x + iter.x, dst_offset.y + iter.y);
+			pixel_put(__get_engine()->win[0], dst, color);
 			iter.x++;
 		}
 		iter.y++;
--- sources/hud/init_minimap.c
+++ sources/hud/init_minimap.c
@@ -6,7 +6,7 @@
 /*   By: jbrousse <jbrousse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 15:47:46 by jbrousse          #+#    #+#             */
-/*   Updated: 2024/05/30 16:47:43 by jbrousse         ###   ########.fr       */
+/*   Updated: 2024/05/31 14:25:21 by jbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ static void	draw_map_texture(t_texture *minimap, t_map *map)
 			if (map->map[z][x] == '1')
 				draw_square(minimap, vector2(z * 10, x * 10),
 					vector2(10, 10), 0x00FF00);
-			else if (map->map[z][x] == '0')
+			else if (map->map[z][x] == '0' || map->map[z][x] == 'N' || map->map[z][x] == 'S' || map->map[z][x] == 'E' || map->map[z][x] == 'W')
 				draw_square(minimap, vector2(z * 10, x * 10),
 					vector2(10, 10), 0xFF0000);
 			else
